41a293b4605e7a345548a5f11a4e2e1755696c7c
Mariela <yordanova.mariela@gmail.com>
2022-05-07 17:22:59 -0400

    uploaded Captains log

--- routes/createLog.js
+++ routes/createLog.js
@@ -1,6 +1,32 @@
 const express = require("express");
 const logRouter = express.Router();
+const LogModel = require('../models/logs')
 
+// insert array ships
+logRouter.post('/', (req, res)=>{
+LogModel.insertMany([
+    {title: "Discovered treasure", entry: 'Morning', shipIsBroken: 'no'},
+    {title: 'Hit the reef', entry: 'night', shipIsBroken: 'yes'},       
+    {title: 'Met pirates', entry: '#', shipIsBroken: 'no'},
+    {title: 'Must Refill rum barrels. Critically low levels', entry: 'dinner time', shipIsBroken: 'no'},
+    {title: 'Rum is gone!!!The crew threatens with mutiny', entry: 'evening', shipIsBroken: 'no'},
+    {title: 'Mutiny on board. Had to share my treasure with these rats', entry: 'midnight', shipIsBroken: 'no'}
+    
+], (err, LogEntry)=>{
+    if(err) {
+        res.status(400).json({message: err.message})
+    }else{
+        res.status(200).json({message: LogEntry })
+    }
+})
+    })
+    
+
+
+
+// logRouter.create("/new", (req, res)=>{
+//     newShips=req.body
+// })
 
 
 
