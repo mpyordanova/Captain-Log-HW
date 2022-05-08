3fa32e780ba8066f6373abe4ccaee8502b6d18c5
Mariela <yordanova.mariela@gmail.com>
2022-05-08 13:40:04 -0400

    delete and index routes

--- routes/createLog.js
+++ routes/createLog.js
@@ -13,23 +13,7 @@ const LogEntry = [
 ]
 
 // insert array ships
-logRouter.post('/', (req, res)=>{
-LogModel.insertMany([
-    {title: "Discovered treasure", entry: 'Morning', shipIsBroken: 'no'},
-    {title: 'Hit the reef', entry: 'night', shipIsBroken: 'yes'},       
-    {title: 'Met pirates', entry: '#', shipIsBroken: 'no'},
-    {title: 'Must Refill rum barrels. Critically low levels', entry: 'dinner time', shipIsBroken: 'no'},
-    {title: 'Rum is gone!!!The crew threatens with mutiny', entry: 'evening', shipIsBroken: 'no'},
-    {title: 'Mutiny on board. Had to share my treasure with these rats', entry: 'midnight', shipIsBroken: 'no'}
-    
-], (err, LogEntry)=>{
-    if(err) {
-        res.status(400).json({message: err.message})
-    }else{
-        res.status(200).json({message: LogEntry })
-    }
-})
-    })
+
     
 // update some mistakes in the initial entry
 logRouter.put('/update/:id', (req, res)=>{
